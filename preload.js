101da6d9ae923c05026c6010d14b82ba66f832d1
chuancyzhang <3195234362@qq.com>
2026-03-22 10:11:01 +0800

    清理 Electron 痕迹

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    saveFile: (filename, content) => ipcRenderer.invoke('save-file', { filename, content }),
-    // 这里可以暴露更多 Node.js 能力给前端，例如读取特定目录
-});
